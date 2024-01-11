d588db605dd505b1bf032c57027acd6e09b1657f
Oli Young <oli.young@immutable.com>
2024-01-11 10:48:09 +1030

    Adding Knock as backend PNS

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -2,10 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { kv } from "@vercel/kv";
 
+import { Knock } from "@knocklabs/node";
+
 export async function POST(request: NextRequest) {
   const eventBody = await request.json();
   const messageBody = JSON.parse(eventBody.Message);
   const url = `https://api.sandbox.immutable.com/v1/chains/imtbl-zkevm-testnet/collections/${messageBody.data.contract_address}/nfts/${messageBody.data.token_id}/owners?page_size=1`;
+  const knock = new Knock(process.env.KNOCK_API_KEY);
   try {
     const response = await fetch(url, {
       method: "GET",
@@ -14,7 +17,12 @@ export async function POST(request: NextRequest) {
     const data = await response.json();
     const walletAddress = data.result[0].account_address;
     await kv.sadd(walletAddress, JSON.stringify(messageBody.data));
+    await knock.workflows.trigger("imtblzkevmnft", {
+      recipients: [walletAddress],
+      data: messageBody,
+    });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ status: 500 });
   }
   return NextResponse.json({ status: 201 });
